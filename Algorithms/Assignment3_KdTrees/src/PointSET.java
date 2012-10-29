3872a791c8a368e5bcbf70da960a1005ce05723a
Guðjón Magnússon <geinarm@gmail.com>
2012-10-29 17:56:44 +0000

    added some tests and helper classes

--- Algorithms/Assignment3_KdTrees/src/PointSET.java
+++ Algorithms/Assignment3_KdTrees/src/PointSET.java
@@ -65,36 +65,43 @@ public class PointSET {
         RectHV[] rectangles = new RectHV[nrOfRecangles];
         Point2D[] pointsCont = new Point2D[nrOfPointsCont];
         Point2D[] pointsNear = new Point2D[nrOfPointsNear];
+        
         for(int i = 0; i < nrOfRecangles; i++)
         {
             rectangles[i] = new RectHV(StdIn.readDouble(),StdIn.readDouble(),StdIn.readDouble(),StdIn.readDouble());
         }
+        
         for(int i = 0; i < nrOfPointsCont; i++)
         {
             pointsCont[i] = new Point2D(StdIn.readDouble(),StdIn.readDouble());
         }
+        
         for(int i = 0; i < nrOfPointsNear; i++)
         {
             pointsNear[i] = new Point2D(StdIn.readDouble(),StdIn.readDouble());
         }
+        
         PointSET set = new PointSET();
         for (int i = 0; !StdIn.isEmpty(); i++) {
             double x = StdIn.readDouble(), y = StdIn.readDouble();
             set.insert(new Point2D(x,y));
         }
+        
         for(int i = 0; i < nrOfRecangles; i++)
         {
 		    // Query on rectangle i, sort the result, and print
 		    Iterable<Point2D> ptset = set.range(rectangles[i]);
 		    int ptcount=0;
 		    for (Point2D p : ptset)
-			ptcount++;
+		    	ptcount++;
+    		
 		    Point2D[] ptarr = new Point2D[ptcount];
 		    int j=0;
 		    for (Point2D p : ptset) {
-			ptarr[j] = p;
-			j++;
+		    	ptarr[j] = p;
+		    	j++;
 		    }
+		    
 		    Arrays.sort(ptarr);
             StdOut.println("Inside rectangle " + (i + 1) + ":"); 
             for (j=0; j < ptcount; j++)

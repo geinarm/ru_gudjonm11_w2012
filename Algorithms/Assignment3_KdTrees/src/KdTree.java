e4bed5653e43aebdea967f5bf3cf0ce8c596f054
Guðjón Magnússon <geinarm@gmail.com>
2012-10-30 16:04:04 +0000

    Finished PointSet, started KdTree

--- Algorithms/Assignment3_KdTrees/src/KdTree.java
+++ Algorithms/Assignment3_KdTrees/src/KdTree.java
@@ -4,15 +4,22 @@
 import java.util.Arrays;
 
 public class KdTree {
-    // Your code here
-	public KdTree()
-	{
-		
-	}
+    Node root = null;
+    
+    private class Node
+    {
+    	public Point2D value;
+    	public Node left, right;
+    	
+    	public Node(Point2D p)
+    	{
+    		this.value = p;
+    	}
+    }
 	
 	public boolean isEmpty()
 	{
-		return true;
+		return root == null;
 	}
 	
 	public int size()
@@ -22,12 +29,66 @@ public class KdTree {
 	
 	public void insert(Point2D p)
 	{
-		
+		if(!contains(p)){
+			if(root == null) root = new Node(p);
+			else insertAt(root, p, p.x());
+		}
+	}
+	
+	private void insertAt(Node n, Point2D p, double key)
+	{
+		if(key == p.x()){
+			if(key < n.value.x()){
+				if(n.left == null) n.left = new Node(p);
+				else insertAt(n.left, p, p.y());
+			}
+			else{
+				if(n.right == null) n.right = new Node(p);
+				else insertAt(n.right, p, p.y());
+			}
+		}
+		else{
+			if(key < n.value.y()){
+				if(n.left == null) n.left = new Node(p);
+				else insertAt(n.left, p, p.x());
+			}
+			else{
+				if(n.right == null) n.right = new Node(p);
+				else insertAt(n.right, p, p.x());
+			}
+		}
 	}
 	
 	public boolean contains(Point2D p)
 	{
-		return false;
+		return findAt(root, p, p.x());
+	}
+	
+	private boolean findAt(Node n, Point2D p, double key)
+	{
+
+		if(n == null)
+		{
+			return false;
+		}
+		else if(n.value.equals(p))
+		{
+			return true;
+		}
+		else if(key == p.x())
+		{
+			if(key < n.value.x())
+				return findAt(n.left, p, p.y());
+			else
+				return findAt(n.right, p, p.y());
+		}
+		else
+		{
+			if(key < n.value.y())
+				return findAt(n.left, p, p.x());
+			else
+				return findAt(n.left, p, p.x());
+		}
 	}
 	
 	public void draw()
@@ -49,6 +110,32 @@ public class KdTree {
       *  Test client
       ******************************************************************************/
     public static void main(String[] args) { 
+    	
+    	KdTree set = new KdTree();    	
+    	int pointCount = StdIn.readInt();
+    	RectHV rect = new RectHV(0.5, 0.1, 0.8, 0.9);
+    	Point2D point = new Point2D(0.6, 0.7);
+    	
+    	for(int i=0; i < pointCount; i++){
+            double x = StdIn.readDouble(), y = StdIn.readDouble();
+            set.insert(new Point2D(x,y));
+            //StdDraw.filledCircle(x, y, 0.01);
+    	}
+    	
+    	StdOut.println(set.isEmpty());
+    	
+    	StdOut.println(set.contains(new Point2D(0.5, 0.5)));
+    	
+    	/*Iterable<Point2D> inRect = set.range(rect);
+    	for(Point2D p : inRect){
+    		StdOut.println(p);
+    		StdDraw.filledCircle(p.x(), p.y(), 0.02);
+    	}*/
+    	
+    	//Point2D closest = set.nearest(point);
+
+    	
+    	/*
         int nrOfRecangles = StdIn.readInt();
         int nrOfPointsCont = StdIn.readInt();
         int nrOfPointsNear = StdIn.readInt();
@@ -111,6 +198,7 @@ public class KdTree {
         }
         
         StdOut.println();
+        */
     }
    
 }
--- Algorithms/Assignment3_KdTrees/src/PointSET.java
+++ Algorithms/Assignment3_KdTrees/src/PointSET.java
@@ -9,56 +9,125 @@
   *    including rectangle and nearest-neighbor queries
   *
   *************************************************************************/
-
-import java.awt.Point;
+import java.util.ArrayList;
+import java.util.TreeSet;
 import java.util.Arrays;
 
 public class PointSET {
     // Your code here
+	TreeSet<Point2D> bst;
 	
 	public PointSET()
 	{
-		
+		bst = new TreeSet<Point2D>();
 	}
 	
 	public boolean isEmpty()
 	{
-		return true;
+		return bst.isEmpty();
 	}
 	
 	public int size()
 	{
-		return 0;
+		return bst.size();
 	}
 	
+	// add the point p to the set (if it is not already in the set)
 	public void insert(Point2D p)
 	{
-		
+		if(!bst.contains(p))
+			bst.add(p);
 	}
 	
+	// does the set contain the point p?
 	public boolean contains(Point2D p)
 	{
-		return false;
+		return bst.contains(p);
 	}
 	
+	// draw all of the points to standard draw
 	public void draw()
 	{
-		
+		for(Point2D p : bst){
+			StdDraw.filledCircle(p.x() * 512, p.y() * 512, 5);
+		}
 	}
 	
+	// all points in the set that are inside the rectangle
 	public Iterable<Point2D> range(RectHV rect)
 	{
-		return null;
+		ArrayList<Point2D> points = new ArrayList<Point2D>();
+		
+		for(Point2D p : bst)
+		{
+			if(rect.contains(p))
+				points.add(p);
+		}
+		return points;
 	}
 	
+	// a nearest neighbor in the set to p; null if set is empty
    	public Point2D nearest(Point2D p)
    	{
-   		return new Point2D(0.0, 0.0);
+   		if(isEmpty())
+   			return null;
+   		
+   		Point2D best = bst.first();
+   		double distance = p.distanceTo(best);
+   		
+   		for(Point2D point : bst)
+   		{
+   			double d = p.distanceTo(point); 
+   			if(d < distance){
+   				best = point;
+   				distance = d;
+   			}
+   		}
+   		
+   		return best;
    	}
 
    
    	//Test
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
+    	
+    	/*
+    	PointSET set = new PointSET();
+    	int pointCount = StdIn.readInt();
+    	RectHV rect = new RectHV(0.5, 0.1, 0.8, 0.9);
+    	Point2D point = new Point2D(0.6, 0.7);
+
+    	StdDraw.show(0);
+    	StdDraw.clear();
+    	
+    	for(int i=0; i < pointCount; i++){
+            double x = StdIn.readDouble(), y = StdIn.readDouble();
+            set.insert(new Point2D(x,y));
+            StdDraw.filledCircle(x, y, 0.01);
+    	}
+    	
+    	Iterable<Point2D> inRect = set.range(rect);
+    	for(Point2D p : inRect){
+    		StdOut.println(p);
+    		StdDraw.filledCircle(p.x(), p.y(), 0.02);
+    	}
+    	
+    	Point2D closest = set.nearest(point);
+    	
+    	
+    	rect.draw();
+    	StdDraw.setPenColor(StdDraw.RED);
+    	StdDraw.filledCircle(point.x(), point.y(), 0.01);
+    	StdDraw.setPenColor();
+    	StdDraw.line(point.x(), point.y(), closest.x(), closest.y());
+    	StdDraw.filledCircle(closest.x(), closest.y(), 0.02);
+    	
+    	//StdDraw.filledCircle(0.5, 0.5, 0.1);
+    	
+    	StdDraw.show(50);
+    	*/
+    	
+    	
         int nrOfRecangles = StdIn.readInt();
         int nrOfPointsCont = StdIn.readInt();
         int nrOfPointsNear = StdIn.readInt();
@@ -80,13 +149,14 @@ public class PointSET {
         {
             pointsNear[i] = new Point2D(StdIn.readDouble(),StdIn.readDouble());
         }
+    	
         
         PointSET set = new PointSET();
         for (int i = 0; !StdIn.isEmpty(); i++) {
             double x = StdIn.readDouble(), y = StdIn.readDouble();
             set.insert(new Point2D(x,y));
         }
-        
+         
         for(int i = 0; i < nrOfRecangles; i++)
         {
 		    // Query on rectangle i, sort the result, and print
